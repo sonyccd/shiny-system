87adf46832c0ad00e82e730c15a0dff23a8d10bb
Brad Bazemore <bwbazemore@pendo.io>
2021-11-11 16:25:42 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+let auth0 = null;
+const fetchAuthConfig = () => fetch("/auth_config.json");
+// ..
 
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+// ..
+
+window.onload = async () => {
+  await configureClient();
+}
+
+// ..
+
+window.onload = async () => {
+  await configureClient();
+
+  // NEW - update the UI state
+  updateUI();
+};
+
+// NEW
+const updateUI = async () => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+};
